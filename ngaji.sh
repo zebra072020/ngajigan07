62fe6b46531c1bc2496330806a2b42a14a9cc576
zebra072020 <59621492+zebra072020@users.noreply.github.com>
2020-01-08 04:09:49 +0700

    Add files via upload

---
+++ ngaji.sh
@@ -0,0 +1,102 @@
+#ngajigan
+#usr/bin/bash
+figlet "ngaji online"
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+
+
+echo  "$k"_______________________"$ku"
+echo  "$me"   Author : by        "$me•"
+echo  "$me"                      "$me•"
+echo  "$me•" $pur Mr.Pirate      "$me•"
+echo  "$me•" $pur Mr.Zebra       "$me•"
+echo  "$ku"_______________________"$ku"
+echo "selamat datang di ngaji online"
+#userrrr
+text1='mengaji  Bersama'
+
+echo 'Mari Kita '$text1
+#dfgghhj
+
+read -p 'Masukkan Nama ente : ' nama1; 
+
+echo 'assalamualaikum Selamat Datang '$nama1
+#gggggggg
+#!/bin/sh
+echo -n 'Masukkan Password Scrip ini : '
+read pw; 
+
+if [ $pw = 'NGAJIGAN' ];
+then
+    echo 'alhamdulillah Password anda benar'
+else
+    echo 'Password anda salah'
+    echo 'Silakan Masukkan kembali'
+
+fi
+clear
+sleep 1
+figlet "ngaji gan"
+  #menu utama
+  echo '1) ngaji kitab'
+  echo '2) ngaji qur an'
+  echo '3) cara belajar hadroh'
+  echo '0) exit '
+  #masukan atau input
+  read -p 'Masukkan Pilhan anda : ' pil; 
+  #kondi jika input = 1 maka akan menginstall redhawk
+  if [ $pil = '1' ]
+  then
+      clear
+      sleep 3
+      echo 'yah.. yang ini belum jadi'
+      sleep 1
+      echo 'mohon bersabar'
+      sleep 1
+  
+  fi
+
+  #kondisi jika input = 2 maka akan menginstall Cmbah-v2
+  if [ $pil = '2' ];
+  then
+      clear
+      sleep 3
+      echo 'yang ini juga belum jadi na_am yah:('
+      sleep 1
+      echo 'mohon bersabar...!'
+      sleep 1
+
+  fi
+
+  #kondisi jika input = 3 maka akan menginstall LazyGo
+  if [ $pil = '3' ];
+  then
+      clear
+      sleep 3
+      echo 'caranya yaitu seperti ini'
+      sleep 1
+      echo 'metode nanya ; D T T D D D T'
+      echo 'metode jawab : D T T D D T T D T '
+      sleep 1
+  
+  fi
+ 
+ #kondisi jika input = 0 maka akan keluar dari tools
+  if [ $pil = '0' ];
+  then
+      echo 'bye bye.....! '
+      sleep 2
+      exit
+  #kodisi else
+  else
+      echo 'ERROR : cobalah beberapa saat lagi....! '
+      sleep 1
+      echo $ulang
+
+  fi
